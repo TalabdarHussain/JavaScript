3bf67c763a28414bc3fd0691a9741d5ebf610eb9
Talabdar Hussain <111138656+TalabdarHussain@users.noreply.github.com>
2024-04-06 21:53:15 +0500

    BasicsConcept

---
+++ 01_basics/conversion.js
@@ -0,0 +1,14 @@
+// const Name=null
+
+// let newNumber=Number(Name)
+// console.log(newNumber);
+// console.log(typeof newNumber);
+
+
+// let isLogIn= ""
+// let convertedValue= Boolean(isLogIn)
+// console.log(convertedValue);
+
+
+
+console.log(null==2);
---
+++ 01_basics/variable.js
@@ -0,0 +1,8 @@
+const userName="Talabdar"
+let userEmail="talabdar@gmail.com"
+var accountPassword="123"
+
+
+console.table([userEmail,userEmail,accountPassword]);
+
+

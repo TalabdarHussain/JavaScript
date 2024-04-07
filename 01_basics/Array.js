7b16e50762fbd3464ac301c61668695ca4ae995f
Talabdar Hussain <111138656+TalabdarHussain@users.noreply.github.com>
2024-04-07 21:08:49 +0500

    AboutArrays

---
+++ 01_basics/Array.js
@@ -0,0 +1,33 @@
+
+
+let array_1=[1,2,3,4,5,6]
+
+let array_2=[2,3,444,22,33,55]
+
+
+// let another_array=array_1.splice(1,3)
+
+// // console.log(another_array);
+// console.log(array_1);
+
+
+// let another_array=array_1.slice(1,3)
+
+// console.log(another_array);
+// console.log(array_1);
+
+
+let marvel_heros=["thor","IronMan","spiderman"]
+
+let dc_heros=["flash","IronMan","spiderman"]
+
+// let another_array=marvel_heros.push(dc_heros)
+
+// console.log(another_array);
+
+
+// spread
+
+// let another_array=[...marvel_heros,...dc_heros]
+
+// console.log(another_array);
